a26be670ee7021f092302dec4ea0c4c7e4c72eb8
dwikananta <dwikirivan@gmail.com>
2021-08-04 06:57:45 +0700

    change database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('profile_image')->nullable();
             $table->string('token_key')->nullable();
+            $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });

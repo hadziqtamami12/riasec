46981b5ba30fc22df12b3dd5321384d4251a0558
Dwiki Rivan <47978399+siucup@users.noreply.github.com>
2021-08-09 13:53:56 +0700

    link appadmin
    change sidebar admin

--- resources/views/layouts/appadmin.blade.php
+++ resources/views/layouts/appadmin.blade.php
@@ -57,71 +57,60 @@
                <div class="shadow-bottom"></div>
                <ul class="list-unstyled menu-categories" id="accordionExample">
 
-                  {{-- @if (Auth::user()->roles()->first()->name === 'superadmin') --}}
-                     <li class="menu {{ (request()->segment(1) == 'superadmin') ? 'active' : ''  }}">
-                        <a href="{{ route('roleAdmin') }}" aria-expanded="{{ (request()->segment(1) == 'superadmin') ? 'true' : 'false' }}" class="dropdown-toggle">
-                              <div class="">
-                                 <i data-feather="trello"></i>
-                                 <span>Recap Kunjungan</span>
-                              </div>
-                        </a>
-                     </li>                      
-                  {{-- @else
-                     <li class="menu {{ (request()->segment(1) == 'admin') ? 'active' : '' }}">
-                        <a href="{{ route('roleAdmin') }}" aria-expanded="{{ (request()->segment(1) == 'admin') ? 'true' : 'false' }}" class="dropdown-toggle">
-                              <div class="">
-                                 <i data-feather="trello"></i>
-                                 <span>Recap Kunjungan</span>
-                              </div>
-                        </a>
-                     </li>  
-                  @endif --}}
+                  <li class="menu {{ (request()->segment(1) == 'admin') ? 'active' : ''  }}">
+                     <a href="{{ route('roleAdmin') }}" aria-expanded="{{ (request()->segment(1) == 'admin') ? 'true' : 'false' }}" class="dropdown-toggle">
+                        <div class="">
+                           <i data-feather="trello"></i>
+                           <span>Recap Kunjungan</span>
+                        </div>
+                     </a>
+                  </li>                      
 
                   <li class="menu {{ (request()->segment(1) == 'soal') ? 'active' : '' }}">
                      <a href="{{ route('soal.index') }}" aria-expanded="{{ (request()->segment(1) == 'soal') ? 'true' : 'false' }}" class="dropdown-toggle">
-                           <div class="">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                 stroke-linejoin="round" class="feather feather-clipboard">
-                                 <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2">
-                                 </path>
-                                 <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
-                              </svg>
-                              <span>Soal &AMP; Jawaban</span>
-                           </div>
+                        <div class="">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                              stroke-linejoin="round" class="feather feather-clipboard">
+                              <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2">
+                              </path>
+                              <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+                           </svg>
+                           <span>Soal &AMP; Jawaban</span>
+                        </div>
                      </a>
                   </li>
 
                   <li class="menu {{ (request()->segment(1) == 'tipekep') ? 'active' : '' }}">
                      <a href="{{ route('tipekep.index') }}" aria-expanded="{{ (request()->segment(1) == 'tipekep') ? 'true' : 'false' }}" class="dropdown-toggle">
-                           <div class="">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                 stroke-linejoin="round" class="feather feather-layers">
-                                 <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
-                                 <polyline points="2 17 12 22 22 17"></polyline>
-                                 <polyline points="2 12 12 17 22 12"></polyline>
-                              </svg>
-                              <span>Tipe Kepribadian</span>
-                           </div>
+                        <div class="">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                              stroke-linejoin="round" class="feather feather-layers">
+                              <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
+                              <polyline points="2 17 12 22 22 17"></polyline>
+                              <polyline points="2 12 12 17 22 12"></polyline>
+                           </svg>
+                           <span>Tipe Kepribadian</span>
+                        </div>
                      </a>
                   </li>
 
                   <li class="menu {{ (request()->segment(1) == 'programstudi') ? 'active' : '' }}">
                      <a href="{{ route('programstudi.index') }}" aria-expanded="{{ (request()->segment(1) == 'programstudi') ? 'true' : 'false' }}" class="dropdown-toggle">
-                           <div class="">
-                              <i data-feather="user-check"></i>
-                              <span>Program Studi</span>
-                           </div>
+                        <div class="">
+                           <i data-feather="user-check"></i>
+                           <span>Program Studi</span>
+                        </div>
                      </a>
                   </li>
 
                   <li class="menu {{ (request()->segment(1) == 'account') ? 'active' : '' }}">
                      <a href="{{route('account.index')}}" aria-expanded="{{ (request()->segment(1) == 'account') ? 'true' : 'false' }}" class="dropdown-toggle">
-                           <div class="">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                              <span>Daftar Pengguna</span>
-                           </div>
+                        <div class="">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                           <span>Daftar Pengguna</span>
+                        </div>
                      </a>
                   </li>
                </ul>

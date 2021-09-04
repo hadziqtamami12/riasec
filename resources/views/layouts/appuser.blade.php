8e477971ab043239028fe8f9f2252a7a3b3f6166
Dwiki Rivan <47978399+siucup@users.noreply.github.com>
2021-09-04 12:44:04 +0700

    .

--- resources/views/layouts/appuser.blade.php
+++ resources/views/layouts/appuser.blade.php
@@ -27,10 +27,10 @@
    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/users/account-setting.css')}}"/>
    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/elements/alert.css')}}">
    {{-- SweatAlert --}}
-   <link rel="stylesheet" type="text/css" href="{{asset('plugins/animate/animate.css')}}"/>
-   <link rel="stylesheet" type="text/css" href="{{asset('plugins/sweetalerts/sweetalert2.min.css')}}" />
+   {{-- <link rel="stylesheet" type="text/css" href="{{asset('plugins/animate/animate.css')}}"/> --}}
+   {{-- <link rel="stylesheet" type="text/css" href="{{asset('plugins/sweetalerts/sweetalert2.min.css')}}" />
    <link rel="stylesheet" type="text/css" href="{{asset('plugins/sweetalerts/sweetalert.css')}}" />
-   <link rel="stylesheet" type="text/css" href="{{asset('assets/css/components/custom-sweetalert.css')}}" />
+   <link rel="stylesheet" type="text/css" href="{{asset('assets/css/components/custom-sweetalert.css')}}" /> --}}
    <!-- END GLOBAL MANDATORY STYLES -->
 </head>
 <body class="alt-menu" data-spy="scroll" data-offset="50">
@@ -86,9 +86,9 @@
       feather.replace();
    </script>
    {{-- SweatAlert --}}
-   <script src="{{asset('plugins/sweetalerts/sweetalert2.min.js')}}"></script>
+   {{-- <script src="{{asset('plugins/sweetalerts/sweetalert2.min.js')}}"></script>
    <script src="{{asset('plugins/sweetalerts/custom-sweetalert.js')}}"></script>
-   <script src="{{asset('plugins/sweetalerts/promise-polyfill.js')}}"></script>
+   <script src="{{asset('plugins/sweetalerts/promise-polyfill.js')}}"></script> --}}
    <!-- END GLOBAL MANDATORY STYLES -->
    <!-- BEGIN PAGE LEVEL SCRIPTS -->
    @yield('trigger')
